7f87afd2a0367d63cd42c6c2ff23fb5060e6b654
Karthikeyan <kamtechie@gmail.com>
2020-01-26 08:12:37 +0000

    added fcm send

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-var admin = require("firebase-admin");
+const fetch = require('node-fetch');
+const config = require('./config.json')
 
-var serviceAccount = require("./manmethacks2020-ngofundtracker-firebase-adminsdk-nxfkc-9da25aab61.json");
+function sendFCMNotif(title, body, token) {
+  fetch("https://fcm.googleapis.com/fcm/send", {
+    method: 'post',
+    body: JSON.stringify(
+      {
+        "notification": {
+          "title": title,
+          "body": body,
+          "click_action": "http://localhost:3000/",
+          "icon": "http://localhost:3000/favicon.ico"
+        },
+        "to": token
+      }
+    ),
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization":"key=" + config["fb_server_key"]
+    },
+  }).then((res) => console.log("notif sent"))
+}
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://manmethacks2020-ngofundtracker.firebaseio.com"
-}); 
 
+// sendFCMNotif("title", "body", "cw4tml9r6drj-acOYLebkT:APA91bHL2O-JQsgbRr4BWF5Q3evzf_o07-Vozn8Y8FfXHuRb__j_caedJ8xQ7oThzkpYotEUmviWZeBcOBvG6y1t6ykTNPFTGJtfpHAdqqG7G1VE7wsKcFChFXGYZir6VZA_fsNY1iV3")
